45740055634bc67e0072a4bf0e4ea0134fef682f
zgswlaws <858964992@qq.com>
2019-10-10 23:03:43 +0800

    firt python programe

---
+++ cqh.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time : 2019/10/10 22:55
+# @Author : cqh
+# @file : cqh.py
+# @Software " PyCharm
+# 第一次用 Pycharm edit python programe No. one.
+print('Hello world!')
